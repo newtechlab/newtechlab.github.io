314f7ed7193a3a17ad3be9ec9b6c3415021fe07d
Viktor <viktor.ogeman@gmail.com>
2018-04-24 16:48:37 +0200

    ie 11

--- radartest/radar.js
+++ radartest/radar.js
@@ -1,48 +1,48 @@
 var radar = function (container, data) {
-    let w, h;
-    let aX, aY, aS;
+    var w, h;
+    var aX, aY, aS;
 
-    let sr = 250;
-    let radius = 350;
+    var sr = 250;
+    var radius = 350;
 
-    let init = () => {
+    var init = function() {
         w = window.innerWidth;
         h = window.innerHeight;
         container.innerHtml = ""
-        data.map((uni, i) => cUni(uni, i))
+        data.map(function(uni, i) { cUni(uni, i)})
 
-        aX = animate(500, (x) => container.style.transform = "translateX("+x*0.1+"px) scale("+(1+x/100)+")")
+        aX = animate(500, function(x) { container.style.transform = "translateX("+x*0.1+"px) scale("+(1+x/100)+")"})
         
         //aX.update(100)
         //zoom(0) // show the starting point
 
         // Set up the animation we will be using
-    }, cUni = (uni, i) => {
-        let n = document.createElement("div")
+    }, cUni = function(uni, i) {
+        var n = document.createElement("div")
         n.className = "universe"
-        uni.map(art => cPlan(n, art))
-        let a = 180*i;
+        uni.map(function(art){ cPlan(n, art)})
+        var a = 180*i;
         n.style.transform = "translateY(-"+radius+"px) rotate("+a+"deg) translateY("+radius+"px)"
         container.appendChild(n)
-    }, cPlan = (p, planet) => {
-        let n = document.createElement("div")
+    }, cPlan = function(p, planet) {
+        var n = document.createElement("div")
         n.className = "planet"        
-        planet.articles.map(art => cArt(n, art))
+        planet.articles.map(function(art){ cArt(n, art)})
         p.appendChild(n)
-    }, cArt = (p, art) => {
-        let n = document.createElement("div")
+    }, cArt = function(p, art) {
+        var n = document.createElement("div")
         n.className = "article"        
         n.innerHTML = art + ""
-        let nn = document.createElement("img")
+        var nn = document.createElement("img")
         nn.src = "https://upload.wikimedia.org/wikipedia/commons/d/d2/Firefox_Logo%2C_2017.png"
         nn.width = 25
         nn.height = 25
         n.appendChild(nn)
         p.appendChild(n)
-    }, zoom = (universe, planet, category) => {
+    }, zoom = function(universe, planet, category){
         if(planet == undefined) {
             // movign between universes, figure out the target center position for this one.
-            let r = Math.random()*w
+            var r = Math.random()*w
             aX.update(r)
 
         } else {
